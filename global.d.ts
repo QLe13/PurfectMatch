c967459b15781b254357c98b4540f4f9b7b682fe
Quang <qle13@Qs-MacBook-Air-4.local>
2023-04-24 19:28:30 -0500

    Small fix

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,10 @@
-declare type Coordination = [number, number];
+declare type Coordinate = [number, number];
 
 declare type Pet = {
   name: string;
   age: number;
   type: string;
   price: number;
-  location: Coordination;
+  location: Coordinate;
 };
 
